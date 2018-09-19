743daba536f9cd42ba59f2caf7c6dd28d986b3bf
ayaulymChinaliyeva <37076155+ayaulymChinaliyeva@users.noreply.github.com>
2018-09-19 21:39:05 +0600

    Committing the solutions on arrays and matrix
    There were given easy problems on arrays, which are very basic, however the algorythms used to solve such problems underly most of the modern solutions and not always understood by people, so here are simple ways of implementing arrays. All in one sln file, also on C#))

---
+++ TaskMatrixes/TaskMatrixes/Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace TaskMatrixes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int row1 = int.Parse(Console.ReadLine());
+            int column1 = int.Parse(Console.ReadLine());
+            int row2 = int.Parse(Console.ReadLine());
+            int column2 = int.Parse(Console.ReadLine());
+            int[,] matrix1 = new int[row1, column1];
+            int[,] matrix2 = new int[row2, column2];
+            for (int i = 0; i < row1; i++)
+            {
+                for (int j = 0; j < column1; j++)
+                {
+                    matrix1[i, j] = int.Parse(Console.ReadLine());
+                }
+            }
+            for (int i = 0; i < row2; i++)
+            {
+                for (int j = 0; j < column2; j++)
+                {
+                    matrix2[i, j] = int.Parse(Console.ReadLine());
+                }
+            }
+            if (column1 == row2)
+            {
+                int[,] matrix3 = new int[row2, column1];
+                for (int i = 0; i < row2; i++)
+                {
+                    for (int j = 0; j < column1; j++)
+                    {
+                        matrix3[i,j] = 0;
+                        for(int k=0; k<column1; k++)
+                        {
+                            matrix3[i, j] = matrix3[i, j] + matrix1[i, k] * matrix2[k, j];
+                            Console.WriteLine(matrix3[i, j]);
+                        }
+                    }
+                }
+                int[,] transposedMatrix = new int[column1, row2];
+                Console.WriteLine("Transpose matrix elements:");
+                for (int i = 0; i < column1; i++)
+                {
+                    for (int j = 0; j < row2; j++)
+                    {
+                        transposedMatrix[i, j] = matrix3[j, i];
+                    }
+                }
+                for(int d=0; d<column1; d++)
+                {
+                    for(int h=0; h<row2; h++)
+                    {
+                        Console.WriteLine(transposedMatrix[d, h]);
+                    }
+                }
+                int[,] frequencies = new int[row2, column1];
+                for(int c=0; c<row2; c++)
+                {
+                    for(int b=0; b<column1; b++)
+                    {
+                        frequencies[c, b] = 0;
+                    }
+                }
+                foreach(int s in matrix3)
+                {
+                    for(int i=0; i<row2; i++)
+                    {
+                        for(int j=0; j<column1; j++)
+                        {
+                            if(s==matrix3[i, j])
+                            {
+                                frequencies[i, j] += 1;
+                            }
+                        }
+                    }
+                }
+                Console.WriteLine("Frequencies of the elements (appear in accordance with indiecies)");
+                for(int k=0; k<row2; k++)
+                {
+                    for(int g=0; g<column1; g++)
+                    {
+                        Console.WriteLine(frequencies[k, g]);
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("Matrixes cannot be multiplied");
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Tasks/Tasks/Program.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tasks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*int n = int.Parse(Console.ReadLine());
+            int[] array = new int[n];
+            for(int i=0; i<n; i++)
+            {
+                array[i] = int.Parse(Console.ReadLine());
+            }
+            int temp = 0;
+            for(int i=0; i<n-1; i++)
+            {
+                for(int j=0; j<n-i-1; j++) { 
+                    if (array[j] > array[j + 1])
+                    {
+                        temp = array[j];
+                        array[j] = array[j + 1];
+                        array[j + 1] = temp;
+                    }
+                }
+            }
+            for(int i=0; i<n; i++)
+            {
+                Console.WriteLine(array[i]);
+            }
+            Console.ReadKey();*/
+            /*!---Task 1 here---!
+            int n = int.Parse(Console.ReadLine());
+            int[] array = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                array[i] = int.Parse(Console.ReadLine());
+            }
+            int sum = 0;
+            for (int i = 0; i < n; i++)
+            {
+                sum += array[i];
+            }
+            Console.WriteLine(sum);
+            Console.ReadKey();*/
+
+            /*!---Task 2 is here---!
+            int n = int.Parse(Console.ReadLine());
+            int[] array = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                array[i] = int.Parse(Console.ReadLine());
+            }
+            int index1 = 0;
+            int index2 = 0;
+            for(int i=1; i<n-1; i++)
+            {
+                if (array[i] + array[i - 1] > array[i] + array[i + 1])
+                {
+                    index1 = i;
+                    index2 = i - 1;
+                }
+                else
+                {
+                    index1 = i;
+                    index2 = i + 1;
+                }
+            }
+            Console.WriteLine("Index 1 is: "+index1+" "+"Index 2 is: "+index2);
+            Console.ReadKey();*/
+
+            /*!---Third task is here---!
+            int n = int.Parse(Console.ReadLine());
+            int[] array = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                array[i] = int.Parse(Console.ReadLine());
+            }
+            List<int> list = new List<int>();
+            for (int i=0; i<n-1; i++)
+            {
+                if (array[i] > array[i + 1])
+                {
+                    list.Add(i);
+                }
+            }
+            int temp = 0;
+            for(int i=0; i< list.Count - 1; i++)
+            {
+                for (int j=0; j< list.Count - 1-i; j++)
+                {
+                    if (list[j] > list[j+1])
+                    {
+                        temp = list[j];
+                        list[j] = list[j+1];
+                        list[j + 1] = temp;
+                    }
+                }
+            }
+            Console.WriteLine("Indiecies are");
+            list.ForEach(Console.WriteLine);
+            Console.ReadKey();*/
+
+            /*!---Fourth task is here---!
+            int n = int.Parse(Console.ReadLine());
+            int[] array = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                array[i] = int.Parse(Console.ReadLine());
+            }
+            for(int i=1; i<n-1; i++)
+            {
+                if(array[i]<array[i+1] && array[i] < array[i - 1])
+                {
+                    Console.WriteLine(i);
+                    break;
+                }
+            }
+            Console.ReadKey();*/
+
+            /*!---Fifth task is here---!
+            int n = int.Parse(Console.ReadLine());
+            int[] array = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                array[i] = int.Parse(Console.ReadLine());
+            }
+            for (int i = n-2; i>=1; i--)
+            {
+                if (array[i] >array[i + 1] && array[i] > array[i - 1])
+                {
+                    Console.WriteLine(i);
+                    break;
+                }
+            }
+            Console.ReadKey();*/
+
+            /*!---Sixth task is here---!
+            int n = int.Parse(Console.ReadLine());
+            int[] array = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                array[i] = int.Parse(Console.ReadLine());
+            }
+            int temp = 0;
+            for(int i=0; i<n-1;i++)
+            {
+                for(int j = 0; j < n - 1 - i; j++)
+                {
+                    if (array[j] > array[j + 1])
+                    {
+                        temp = array[j];
+                        array[j] = array[j + 1];
+                        array[j + 1] = temp;
+                    }
+                }
+            }
+            Console.WriteLine(array[n - 2]);
+            Console.ReadKey();*/
+        }
+    }
+}
